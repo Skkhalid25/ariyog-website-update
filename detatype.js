fa30ea5e863509d9250e62d1b46897b56aa5d646
khalid <web.designer@ariyog>
2025-04-24 14:53:49 +0530

    new update

--- detatype.js
+++ detatype.js
@@ -1,32 +1,35 @@
-// non premetive
-// string, number,boolean,undefine,Null,
+// const prompt = require ("prompt-sync")();
+// // non premetive
+// // string, number,boolean,undefine,Null,
 
-// premitive
-// object,array
+// // premitive
+// // object,array
 
-let name = "khalid";
-let age = 18;
-let pass = true;
-let neww = 
-student = {
-    age:28,
-    name:"sk",
-    class:21
-};
-let student1 = ["ariz","hamza","shk"];
-console.log(name,"type of deta :-",typeof name);
-console.log(age,"type of deta :-",typeof age);
-console.log(pass,"type of deta :-",typeof pass);
-console.log(neww,"type of deta :-",typeof neww);
-console.log(student,"type of deta :-",typeof student);
-console.log(student1,"type of deta :-",typeof student1);
-let y = 10 > 10
-console.log(y);
-// && (and)
-if (age >= 18) {
-    console.log("verify")
-} else if (age < 18){
-console.log("not verify")
-} else {
-    console.log("invalid")
-}
+// let name = "khalid";
+// let age = 18;
+// let pass = true;
+// let neww = 
+// student = {
+//     age:28,
+//     name:"sk",
+//     class:21
+// };
+// let student1 = ["ariz","hamza","shk"];
+// console.log(name,"type of deta :-",typeof name);
+// console.log(age,"type of deta :-",typeof age);
+// console.log(pass,"type of deta :-",typeof pass);
+// console.log(neww,"type of deta :-",typeof neww);
+// console.log(student,"type of deta :-",typeof student);
+// console.log(student1,"type of deta :-",typeof student1);
+// let y = 10 > 10
+// console.log(y);
+// // && (and)
+// if (age >= 18) {
+//     console.log("verify")
+// } else if (age < 18){
+// console.log("not verify")
+// } else {
+//     console.log("invalid")
+// }
+// let f = prompt("");
+// console.log(f);
