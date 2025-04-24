a3302e990ecb12790b9713d691c1d70f2fedf904
khalid <web.designer@ariyog>
2025-04-24 09:57:22 +0530

    new some testing

--- detatype.js
+++ detatype.js
@@ -5,7 +5,7 @@
 // object,array
 
 let name = "khalid";
-let age = 22;
+let age = 18;
 let pass = true;
 let neww = 
 student = {
@@ -20,3 +20,13 @@ console.log(pass,"type of deta :-",typeof pass);
 console.log(neww,"type of deta :-",typeof neww);
 console.log(student,"type of deta :-",typeof student);
 console.log(student1,"type of deta :-",typeof student1);
+let y = 10 > 10
+console.log(y);
+// && (and)
+if (age >= 18) {
+    console.log("verify")
+} else if (age < 18){
+console.log("not verify")
+} else {
+    console.log("invalid")
+}
