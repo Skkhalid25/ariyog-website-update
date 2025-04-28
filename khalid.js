a8ab03a442011fc460f1802b91f0213e401a6813
khalid <web.designer@ariyog>
2025-04-28 18:30:27 +0530

    new ome add

--- khalid.js
+++ khalid.js
@@ -1,2 +1,28 @@
-let a = 5
-console.log(a)
+// let a = 5
+// console.log(a)
+// let x = new Array("khalid","shaikh",23,21)
+// let w = x.toString
+// console.log(typeof w)
+// const myArray = ["shaikh","khalid","taufiq","ahmed"];
+// let check = myArray.toString();
+// console.log(typeof check)
+
+// const text = ["a","d","c","b"];
+// let ucheck = text.sort();
+// console.log(ucheck);
+
+let num = 123456
+let num_one = 123.65
+let num_two = "123456"
+let num_three = "123.45"
+
+    
+// let acheck = parseInt(num_two)
+// console.log(acheck);
+
+// let acheck = parseFloat(num_three)
+// console.log(acheck);
+
+// let a = num_one.toFixed()
+// console.log(a)
+// let a = parsecInt(num-two);
